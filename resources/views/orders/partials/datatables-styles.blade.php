9fa6a6ae202e5e10c3504c572f7fbcadfe1d2213
Hossam <lifesound@live.com>
2017-05-12 06:26:17 +0300

    fixing issues

--- resources/views/orders/partials/datatables-styles.blade.php
+++ resources/views/orders/partials/datatables-styles.blade.php
@@ -1,6 +1,6 @@
-<link type="text/css" rel="stylesheet" href="theme/plugins/datatables/jquery.dataTables.css')">
+<link type="text/css" rel="stylesheet" href="{{asset('theme/plugins/datatables/jquery.dataTables.css')}}">
 <link type="text/css" rel="stylesheet"
-      href="theme/plugins/datatables/extensions/responsive/css/dataTables.responsive.css">
+      href="{{asset('theme/plugins/datatables/extensions/responsive/css/dataTables.responsive.css')}}">
 <link type="text/css" rel="stylesheet"
       href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css">
 <link type="text/css" rel="stylesheet" href="{{asset('theme/partials/dtables.css')}}">
